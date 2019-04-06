cd99722aec25d4ca71cf11d19c98d727ec963d2b
Chad Johnston <cjohnston@megatome.com>
2019-04-06 15:57:24 -0600

    Remove hack for Safari
    Fixes #38

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -8,6 +8,7 @@ import {Item} from "../model/item";
 import {Set, Map} from "immutable";
 import {Util} from "../common/Util";
 import {Catalog} from "../model/catalog";
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: "mt-order",
@@ -24,25 +25,12 @@ export class OrderComponent implements OnChanges {
 
   @ViewChild("orderTotalElement") orderTotalElement: ElementRef;
 
-  orderTotal = 0;
-
-  private isSafari = false;
-
-  constructor() {
-    // Ugly browser detection hack, but we only want to perform the force redraw for Safari
-    const ua = window.navigator.userAgent;
-    this.isSafari = ua.indexOf("Safari") !== -1 && ua.indexOf("Chrome") === -1;
-  }
+  orderTotal = new BehaviorSubject<number>(0);
 
   ngOnChanges(changes: SimpleChanges): void {
     const orderValue = changes.hasOwnProperty("order") ? changes.order.currentValue : this.order;
     const catalogValue = changes.hasOwnProperty("catalog") ? changes.catalog.currentValue : this.catalog;
-    this.orderTotal = Util.getOrderTotal(orderValue, catalogValue);
-    // Safari won"t automatically redraw the total for some reason, so use this ugly hack to force it
-    if (this.isSafari) {
-      this.orderTotalElement.nativeElement.style.display = "none";
-      setTimeout(() => this.orderTotalElement.nativeElement.style.removeProperty("display"), 0);
-    }
+    this.orderTotal.next(Util.getOrderTotal(orderValue, catalogValue));
   }
 
   removeItemFromOrder(categoryId: string, itemId: string) {
